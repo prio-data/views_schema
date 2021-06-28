a9241c61205e4a065ddb71bbfe6d95b22e2a9caf
peder2911 <pglandsverk@gmail.com>
2021-06-28 10:04:40 +0200

    Added models for views_docs

--- views_schema.py
+++ views_schema.py
@@ -1,6 +1,14 @@
-
-from typing import List,Optional
+"""
+The classes in this module define the (json) schema which services and tools
+use to communicate.
+"""
+import datetime
+from typing import List,Optional,Dict
 import pydantic
+
+# =QUERYSET MANAGER MODELS================================
+# These models are used by the queryset_manager service,
+# and the viewser CLI when communicating about querysets.
 
 class Operation(pydantic.BaseModel):
     """
@@ -55,3 +63,50 @@ class Queryset(pydantic.BaseModel):
     description: Optional[str]
 
     operations: List[List[Operation]]
+
+# =DOCS MODELS============================================
+# These models are accepted and returned by the views_docs
+# service, and are returned by various services that
+# expose documentation through the service.
+
+class DocumentationEntry(pydantic.BaseModel):
+    """
+    Services that expose endpoints that return documentation entries
+    can be documented through the views_docs service.
+
+    Note that the entry is recursive: entries can refer to child
+    entries, such as tables > columns.
+    """
+    name: str
+    path: str = "."
+    entries: List["DocumentationEntry"] = []
+    data: Dict[str,str] = {}
+
+class PostedDocumentationPage(pydantic.BaseModel):
+    """
+    The schema for posting a documentationentry to
+    the views_docs service.
+    """
+    content: str
+
+class DocumentationPageListEntry(pydantic.BaseModel):
+    """
+    The schema used when listing documentationentries.
+    """
+    name: str
+    category: str
+    last_edited: datetime.datetime
+    author: str = ""
+
+class DocumentationPageDetail(DocumentationPageListEntry, PostedDocumentationPage):
+    """
+    The schema for showing a full documentation entry, with both content and metadata
+    """
+
+class ViewsDoc(pydantic.BaseModel):
+    """
+    The data returned by views_docs, combining a remote entry with
+    an optional documentation page.
+    """
+    entry: DocumentationEntry
+    page: Optional[DocumentationPageDetail] = None
