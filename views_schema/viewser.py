2e852a7e1c3643f0f2d5e7fa586e58b6a9385478
peder2911 <pglandsverk@gmail.com>
2021-12-21 17:37:16 +0100

    changed viewser schema

--- views_schema/viewser.py
+++ views_schema/viewser.py
@@ -1,14 +1,20 @@
 
+import enum
 import datetime
 from typing import List
 import pydantic
 
-class ErrorMessage(pydantic.BaseModel):
-    title:   str
-    content: str
+class MessageType(enum.Enum):
+    HINT    = 1
+    MESSAGE = 2
+    DUMP    = 3
+
+class Message(pydantic.BaseModel):
+    content:    str
+    message_type: MessageType = MessageType.MESSAGE
 
 class Dump(pydantic.BaseModel):
     title:     str
     timestamp: datetime.datetime
-    username:  str
-    messages:  List[ErrorMessage]
+    username:  str = "anonymous"
+    messages:    List[Message]
