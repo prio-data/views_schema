912cc95568e08df709210cf9fc8a3d150d239b2f
peder2911 <pglandsverk@gmail.com>
2022-01-25 15:49:47 +0100

    Docstring for model object metadata model

--- views_schema/models.py
+++ views_schema/models.py
@@ -3,8 +3,23 @@ import datetime
 import pydantic
 
 class ModelMetadata(pydantic.BaseModel):
+    """
+    ModelMetadata
+    =============
+
+    Data used to organize model objects.
+
+    parameters:
+        author (str): Name of the user that authored the model object.
+        run_id (str): Name of the associated run
+        queryset_name (str): Name of the queryset used to train the model
+        train_start (int): Month identifier for training start date
+        train_start (int): Month identifier for training end date
+        training_date (datetime.datetime): Timestamp for training date (use datetime.datetime.now())
+
+    """
     author:        str
-    run_id:        int
+    run_id:        str
     queryset_name: str
 
     train_start:   int
